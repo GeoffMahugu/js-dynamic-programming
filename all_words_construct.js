dfd859dccfccb3d8ba9004c9782e7b1671ca5556
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-23 13:53:48 +0300

    Create words memoized solution

--- all_words_construct.js
+++ all_words_construct.js
@@ -21,16 +21,18 @@ function createAllWords(target, wordList) {
 
 // Memoized solution
 function memonCreateAllWords(target, wordList, memo = {}) {
+    if (target in memo) return memo[target];
     if (target === '') return [[]];
     let constructArray = [];
     for (let word of wordList) {
         if (target.indexOf(word) === 0) {
             const suffix = target.slice(word.length);
-            const retrievedWordsArr = createAllWords(suffix, wordList); // [['bc'],['de']]
+            const retrievedWordsArr = memonCreateAllWords(suffix, wordList, memo); // [['bc'],['de']]
             const fullWordArr = retrievedWordsArr.map(arrWords => [word, ...arrWords]) // ['a',['bc'],['de']]
             constructArray.push(...fullWordArr);
         }
     }
+    memo[target] = constructArray;
     return constructArray;
 }
 
@@ -39,10 +41,15 @@ function memonCreateAllWords(target, wordList, memo = {}) {
 
 
 
-test_1 = createAllWords('abcdefgh', ['ab', 'gh', 'abc', 'cd', 'h', 'def']);
-test_2 = createAllWords('bananna', ['an', 'n', 'ban', 'a', 'b']);
-test_3 = createAllWords('test', ['es', 'tt', 'st', 'tes']);
-test_4 = createAllWords('purple', ['purp', 'p', 'ur', 'le', 'purpl']);
+// test_1 = createAllWords('abcdefgh', ['ab', 'gh', 'abc', 'cd', 'h', 'def']);
+// test_2 = createAllWords('bananna', ['an', 'n', 'ban', 'a', 'b']);
+// test_3 = createAllWords('test', ['es', 'tt', 'st', 'tes']);
+// test_4 = createAllWords('purple', ['purp', 'p', 'ur', 'le', 'purpl']);
+
+test_1 = memonCreateAllWords('abcdefgh', ['ab', 'gh', 'abc', 'cd', 'h', 'def']);
+test_2 = memonCreateAllWords('bananna', ['an', 'n', 'ban', 'a', 'b']);
+test_3 = memonCreateAllWords('test', ['es', 'tt', 'st', 'tes']);
+test_4 = memonCreateAllWords('purple', ['purp', 'p', 'ur', 'le', 'purpl']);
 
 console.log(`Test_1 -- `, test_1);
 console.log(`Test_2 -- `, test_2);
