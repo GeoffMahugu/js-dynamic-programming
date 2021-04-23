8bd9183c7b4f9525a4e8e4c0a3b98af6275ad8d9
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-23 12:54:04 +0300

    Memoized can count

--- count_can_construct.js
+++ count_can_construct.js
@@ -1,3 +1,10 @@
+/***
+ * Given a Target word and a list of letters.
+ * Check the number of ways the word can be constructed from the list.
+ * Return Count [int] for result
+ * */
+
+// Recursive Solution
 function countConstruct(target, wordList) {
     if (target === '') return 1;
     let numWaysCount = 0;
@@ -11,10 +18,32 @@ function countConstruct(target, wordList) {
     return numWaysCount;
 }
 
-test_1 = countConstruct('abcdefgh', ['ab', 'gh', 'abc', 'cd', 'h', 'def']);
-test_2 = countConstruct('bananna', ['an', 'n', 'ban', 'a', 'b']);
-test_3 = countConstruct('test', ['es', 'tt', 'st', 'tes']);
-test_4 = countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']);
+//  Memoized Solution
+
+function memoCountConstruct(target, wordList, memo = {}) {
+    if (target in memo) return memo[target];
+    if (target === '') return 1;
+    let numWaysCount = 0;
+    for (let word of wordList) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            const numWays = memoCountConstruct(suffix, wordList, memo);
+            numWaysCount += numWays;
+        }
+    }
+    memo[target] = numWaysCount;
+    return numWaysCount;
+}
+
+// test_1 = countConstruct('abcdefgh', ['ab', 'gh', 'abc', 'cd', 'h', 'def']);
+// test_2 = countConstruct('bananna', ['an', 'n', 'ban', 'a', 'b']);
+// test_3 = countConstruct('test', ['es', 'tt', 'st', 'tes']);
+// test_4 = countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']);
+
+test_1 = memoCountConstruct('abcdefgh', ['ab', 'gh', 'abc', 'cd', 'h', 'def']);
+test_2 = memoCountConstruct('bananna', ['an', 'n', 'ban', 'a', 'b']);
+test_3 = memoCountConstruct('test', ['es', 'tt', 'st', 'tes']);
+test_4 = memoCountConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']);
 
 console.log(`Test_1 -- `, test_1);
 console.log(`Test_2 -- `, test_2);
