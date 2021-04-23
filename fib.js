e1d2ea86000836b8cb8e303ae5d7b8d8ac98667b
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-23 15:47:36 +0300

    tablular fib function

--- fib.js
+++ fib.js
@@ -22,14 +22,11 @@ function memFib(num, memo = {}) {
 // O(n)
 function iterFib(n) {
     const table = Array(n + 1).fill(0);
-    // console.log(table);
     table[1] = 1;
     for (let i = 0; i <= n; i++) {
         table[i + 1] += table[i];
         table[i + 2] += table[i];
     }
-    // console.log(table);
-
     return table[n];
 }
 
---
+++ grid_traveler.js
@@ -0,0 +1,7 @@
+/**
+ * You are a taveler in a 2D grid. You begin at the top-lefy corner and tour
+ * goal is to travel to the bottom right corner. You can only move down/right.
+ *
+ * Q: How many ways can you travel to the goal on a grid with dimensions m*n
+ * Write a function 'gridTravelller(m,n) to do the calculation'
+ * */
